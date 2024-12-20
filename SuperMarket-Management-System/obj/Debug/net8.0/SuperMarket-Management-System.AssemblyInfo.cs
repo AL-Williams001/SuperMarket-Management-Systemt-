059214e0981635ff2c29b5fc95eea5680da4abe6
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-12-20 18:09:33 +0800

    Use Static file middleware to apply bootstrap css styles

--- SuperMarket-Management-System/obj/Debug/net8.0/SuperMarket-Management-System.AssemblyInfo.cs
+++ SuperMarket-Management-System/obj/Debug/net8.0/SuperMarket-Management-System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperMarket-Management-System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b57c19da1737cd49c84a652c375b770418245c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aed560968cd67e18b40339f156ffb64d0f0fb00")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperMarket-Management-System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperMarket-Management-System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
